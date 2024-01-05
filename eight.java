764eae5d3a41f6d6efd4dd16be7d70c98e1c4458
AzizbekRashidov <azizbekrashidovuz@gmail.com>
2024-01-05 21:23:09 +0500

    1.2 java

---
+++ eight.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class eight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N;
+        System.out.println("N = ");N = scanner.nextInt();
+
+        int num, sum = 0;
+        for(int i = 1; i <= N; i++){
+            System.out.print(i + "-son: ");num = scanner.nextInt();
+            sum = sum + num;
+        }
+
+        System.out.println("Yig'indi: " + sum);
+        System.out.println("O'rtacha " + sum / N);
+    }
+}
